9d6dbf465bdc73ae64042abb9e0bbf761da2f06a
Tony Anciaux <tonyanciaux@gmail.com>
2022-04-01 12:13:06 +0200

    updated error handling and host address

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import preprocessing.cleaning_data
 
 app = Flask(__name__)
 
-developer_url = "http://127.0.0.1:5000/"
-
 
 @app.route("/", methods=["GET"])
 def home():
@@ -21,51 +19,44 @@ def predict():
         The expected input should be in the following JSON format: 
         {
         "data": {
-        "area": "MANDATORY [int]",
-        "property-type": "MANDATORY ['apartment' | 'bungalow' | 'chalet' | 'duplex' | 'ground-floor' | 'loft' | 
+        "area": int,
+        "property-type": 'apartment' | 'bungalow' | 'chalet' | 'duplex' | 'ground-floor' | 'loft' | 
         'mansion' | 'master-house' | 'mixed-building' | 'penthouse' | 'residence' | 'studio' | 'triplex' | 
-        'villa' ] [str]",
-        "bedrooms-number": "MANDATORY [int]",
-        "zip-code": "MANDATORY [int]",
-        "garden-area": "OPTIONAL - (leave empty if no garden) [int]",
-        "kitchen": "OPTIONAL ['Not equipped' | 'Partially equipped' | 'Fully equipped' | 'Super equipped'] [str]",
-        "balcony": "OPTIONAL [bool]", 
-        "terrace-area": "OPTIONAL - (leave empty if no terrace) [int]",
-        "facades-number": "OPTIONAL [int]",
-        "building-state": "OPTIONAL ['To be renovated' | 'Normal' | 'Excellent' | 'Fully renovated' | 'New'] [str]"
-        "garage": "OPTIONAL [bool]", 
+        'villa'[str],
+        "bedrooms-number": int,
+        "zip-code": int,
+        "garden-area": Optional[int],
+        "kitchen": Optional['Not equipped' | 'Partially equipped' | 'Fully equipped' | 'Super equipped'][str],
+        "balcony": Optional[bool], 
+        "terrace-area": Optional[int],
+        "facades-number": Optional[int],
+        "building-state": Optional['To be renovated' | 'Normal' | 'Excellent' | 'Fully renovated' | 'New'][str]
+        "garage": Optional[bool], 
         }
         }
-"""
+        """
 
     if request.method == "POST":
         content_type = request.headers.get('Content-Type')
         if content_type == "application/json":
             user_input = request.json
             required_keys = ["area", "property-type", "bedrooms-number", "zip-code"]
-            missing_keys = [k for k, v in user_input.items() if len(v) == 0 and k in required_keys]
+            integer_values = ["area", "bedrooms-number", "zip-code", "garden-area", "terrace_area", "facades_number"]
+            boolean_values = ["balcony", "garage"]
+            missing_keys = [k for k, v in user_input["data"].items() if not v and k in required_keys]
+            should_be_integers = [k for k, v in user_input["data"].items() if not isinstance(v, int) and k in integer_values]
+            #should_be_booleans = [k for k, v in user_input["data"].items() if not isinstance(v, bool) and k in boolean_values]
             if missing_keys:
-                return {"Error": f"Missing data in following required field(s): {missing_keys}."}
+                return {"Error": f"Missing required data in following field(s): {missing_keys}."}
+            elif should_be_integers:
+                return {"Error": f"Please enter an integer in following field(s): {should_be_integers}"}
+            #elif should_be_booleans:
+            #    return {"Error": f"Please enter True or False in following field(s): {should_be_booleans}"}
             else:
-                # TODO: convert expected-integers fields into integers ("try int(field)/ except ValueError" method)
-                # try
-                # return user_input
                 return preprocessing.cleaning_data.preprocess(user_input)
         else:
             return "Input should be in JSON format. Go to /predict for specific details."
 
 
-"""
-        # try:
-        # except ValueError:
-        #     return {"Error": "Expected numbers for {field_name_here}"}
-        
-        
-        #     house_data = {
-        #         # TODO: Input structure & matching description in GET request.method
-        #     }
-        # 
-"""
-
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=5000)
